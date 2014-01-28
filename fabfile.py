6917d5aa11ed22705ca740850083a0f9627edd68
Sajjad Anwar <me@sajjad.in>
2014-01-29 03:44:15 +0530

    Fixes user account, few bugs

--- fabfile.py
+++ fabfile.py
@@ -1,23 +1,23 @@
-from fabric.api import run, cd, settings, hide
+from fabric.api import run, cd, settings, hide, sudo
 from fabric.contrib import files
 from ConfigParser import RawConfigParser
 from StringIO import StringIO
 
 def pull():
 	# Pull changes into server.
-	with cd('/home/sajjad/carto'):
+	with cd('carto'):
 		run('git pull')
 
 def create_xml(project):
 	# Create mapnik xml from carto.
 	# Update the mapnil-style directory with the new xml.
-	with cd('/home/sajjad/carto'):
-		run('/home/sajjad/node_modules/carto/bin/carto %s/project.local.mml > /home/sajjad/%s.xml' % (project, project))
-		run('mv /home/sajjad/%s.xml /home/sajjad/src/mapnik-style' % (project, project))
+	with cd('carto'):
+		run('/home/sajjad/node_modules/carto/bin/carto %s/project.local.mml > /home/moabi/%s.xml' % (project, project))
+		run('mv /home/moabi/%s.xml /home/moabi/src/mapnik-style/%s.xml' % (project, project))
 
 def create_local_mml(project):
 	# Use CartoCC and create project.local.mml
-	with cd('/home/sajjad/carto/%s' % project):
+	with cd('carto/%s' % project):
 		run('/home/mikel/bin/CartoCC/bin/cartocc project.mml /home/mikel/bin/CartoCC/moabi.json > project.local.mml')
 
 def update_renderd_conf(project):
@@ -32,24 +32,19 @@ def update_renderd_conf(project):
 		config.add_section(project)
 		config.set(project, 'URI', '/%s/' % project)
 		config.set(project, 'TILEDIR', '/var/lib/mod_tile')
-		config.set(project, 'XML', '/home/sajjad/src/mapnik-style/%s.xml' % project)
+		config.set(project, 'XML', '/home/moabi/src/mapnik-style/%s.xml' % project)
 		config.set(project, 'HOST', 'localhost')
 		config.set(project, 'TILESIZE', '256')
 		buffer = StringIO()
 		config.write(buffer)
 		files.append('renderd.conf', buffer.getvalue(), use_sudo=True)
 
-def _get_current():
-	# Get current crontab
-    with settings(hide('warnings', 'stdout'), warn_only=True):
-        output = run('crontab -l')
-        return output if output.succeeded else ''
-
 def update_cron(project):
-	# Update crontab
-	current = _get_current()
-	new = current+' | /home/sajjad/src/mod_tile/render_expired --map=%s --min-zoom=8 --touch-from=10 >/dev/null' % project
-	run("echo '%s'|crontab -" % new)
+  # Update crontab
+  with settings(hide('warnings', 'stdout'), warn_only=True):
+    current = sudo('crontab -u sajjad -l')
+  new = current+' | /home/sajjad/src/mod_tile/render_expired --map=%s --min-zoom=8 --touch-from=10 >/dev/null' % project
+  run("echo '%s'|sudo crontab -u sajjad -" % new)
 
 def prepare_server():
 	pull()
@@ -70,9 +65,10 @@ def update(project):
 	deploy(project)
 
 def create(project):
-	prepare_server()
-	create_local_mml(project)
-	update_renderd_conf(project)
-	update_cron(project)
-	deploy(project)
+  prepare_server()
+  create_local_mml(project)
+  create_xml(project)
+  update_renderd_conf(project)
+  update_cron(project)
+  deploy(project)
 
