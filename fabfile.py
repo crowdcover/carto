d8ef99fc6604029881ccad3b3716af2299e92974
Sajjad Anwar <me@sajjad.in>
2014-01-29 21:02:34 +0530

    Preserve config entry case in the parser

--- fabfile.py
+++ fabfile.py
@@ -4,40 +4,41 @@ from ConfigParser import RawConfigParser
 from StringIO import StringIO
 
 def pull():
-	# Pull changes into server.
-	with cd('carto'):
-		run('git pull')
+  # Pull changes into server.
+  with cd('carto'):
+    run('git pull')
 
 def create_xml(project):
-	# Create mapnik xml from carto.
-	# Update the mapnil-style directory with the new xml.
-	with cd('carto'):
-		run('/home/sajjad/node_modules/carto/bin/carto %s/project.local.mml > /home/moabi/%s.xml' % (project, project))
-		run('mv /home/moabi/%s.xml /home/moabi/src/mapnik-style/%s.xml' % (project, project))
+  # Create mapnik xml from carto.
+  # Update the mapnil-style directory with the new xml.
+  with cd('carto'):
+    run('/home/sajjad/node_modules/carto/bin/carto %s/project.local.mml > /home/moabi/%s.xml' % (project, project))
+    run('mv /home/moabi/%s.xml /home/moabi/src/mapnik-style/%s.xml' % (project, project))
 
 def create_local_mml(project):
-	# Use CartoCC and create project.local.mml
-	with cd('carto/%s' % project):
-		run('/home/mikel/bin/CartoCC/bin/cartocc project.mml /home/mikel/bin/CartoCC/moabi.json > project.local.mml')
+  # Use CartoCC and create project.local.mml
+  with cd('carto/%s' % project):
+    run('/home/mikel/bin/CartoCC/bin/cartocc project.mml /home/mikel/bin/CartoCC/moabi.json > project.local.mml')
 
 def update_renderd_conf(project):
-	# Update renderd.conf with the config for the new tileset.
-	with cd('/usr/local/etc/'):
-		config = RawConfigParser()
-		# URI=/redd/
-		# TILEDIR=/var/lib/mod_tile
-		# XML=/home/sajjad/src/mapnik-style/REDD.xml
-		# HOST=localhost
-		# TILESIZE=256
-		config.add_section(project)
-		config.set(project, 'URI', '/%s/' % project)
-		config.set(project, 'TILEDIR', '/var/lib/mod_tile')
-		config.set(project, 'XML', '/home/moabi/src/mapnik-style/%s.xml' % project)
-		config.set(project, 'HOST', 'localhost')
-		config.set(project, 'TILESIZE', '256')
-		buffer = StringIO()
-		config.write(buffer)
-		files.append('renderd.conf', buffer.getvalue(), use_sudo=True)
+  # Update renderd.conf with the config for the new tileset.
+  with cd('/usr/local/etc/'):
+    config = RawConfigParser()
+    config.optionxform = str
+    # URI=/redd/
+    # TILEDIR=/var/lib/mod_tile
+    # XML=/home/sajjad/src/mapnik-style/REDD.xml
+    # HOST=localhost
+    # TILESIZE=256
+    config.add_section(project)
+    config.set(project, 'URI', '/%s/' % project)
+    config.set(project, 'TILEDIR', '/var/lib/mod_tile')
+    config.set(project, 'XML', '/home/moabi/src/mapnik-style/%s.xml' % project)
+    config.set(project, 'HOST', 'localhost')
+    config.set(project, 'TILESIZE', '256')
+    buffer = StringIO()
+    config.write(buffer)
+    files.append('renderd.conf', buffer.getvalue(), use_sudo=True)
 
 def update_cron(project):
   # Update crontab
@@ -47,22 +48,22 @@ def update_cron(project):
   run("echo '%s'|sudo crontab -u sajjad -" % new)
 
 def prepare_server():
-	pull()
+  pull()
 
 def deploy(project):
-	# Stop renderd.
-	run('sudo /etc/init.d/renderd stop')
-	# Remove existing tiles.
-	run('rm -rf /var/lib/mod_tile/%s' % project)
-	# Start renderd.
-	run('sudo /etc/init.d/renderd start')
-	# Restart apache.
-	run('sudo /etc/init.d/apache2 restart')
+  # Stop renderd.
+  run('sudo /etc/init.d/renderd stop')
+  # Remove existing tiles.
+  run('rm -rf /var/lib/mod_tile/%s' % project)
+  # Start renderd.
+  run('sudo /etc/init.d/renderd start')
+  # Restart apache.
+  run('sudo /etc/init.d/apache2 restart')
 
 def update(project):
-	prepare_server()
-	create_xml(project)
-	deploy(project)
+  prepare_server()
+  create_xml(project)
+  deploy(project)
 
 def create(project):
   prepare_server()
