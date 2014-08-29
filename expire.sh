3855ab40652f9f2e7867dd4afb2ce7e040d94052
Sajjad Anwar <me@sajjad.in>
2014-08-29 19:03:08 +0530

    Refactor expiry into shell script, update fab to push tiles to tiles.list

---
+++ expire.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rm /home/moabi/expire.list
+
+# Create replication.
+cd ~/app/replicate;~/app/osmosis/bin/osmosis -q --replicate-apidb authFile=dbAuth.txt allowIncorrectSchemaVersion=true validateSchemaVersion=no --write-replication workingDirectory=data;
+
+# Apply replication and generate expire.list
+~/app/osmosis/bin/osmosis --read-replication-interval workingDirectory=/home/moabi/.osmosis --simplify-change --write-xml-change /home/moabi/changes.osc.gz&&osm2pgsql --append -k --slim -C 1800 --number-processes 6 -d gis -U gis /home/moabi/changes.osc.gz -e15 -o /home/moabi/expire.list;
+
+# Read tiles.list and expire.
+while read -r line
+do
+    cat /home/moabi/expire.list | /home/moabi/src/mod_tile/render_expired --map=$line --min-zoom=2 --touch-from=10
+done < /home/moabi/tiles.list
--- fabfile.py
+++ fabfile.py
@@ -25,7 +25,7 @@ def update_renderd_conf(project):
   # Update renderd.conf with the config for the new tileset.
   # URI=/redd/
   # TILEDIR=/var/lib/mod_tile
-  # XML=/home/moabi/src/mapnik-style/REDD.xml
+  # XML=/home/moabi/src/mppnik-style/REDD.xml
   # HOST=localhost
   # TILESIZE=256
   with cd('/usr/local/etc/'):
@@ -33,12 +33,8 @@ def update_renderd_conf(project):
     config = template.substitute(project=project)
     files.append('renderd.conf', config, use_sudo=True)
 
-def update_cron(project):
-  # Update crontab
-  with settings(hide('warnings', 'stdout'), warn_only=True):
-    current = run('crontab -l')
-  new = current+'cat /home/moabi/expire.list | /home/moabi/src/mod_tile/render_expired --map=%s --min-zoom=8 --touch-from=10 >/dev/null;' % project
-  run("echo '%s'|crontab -" % new)
+def update_tiles_list(project):
+  files.append('/home/moabi/tiles.list', project)
 
 def copy_img():
   with cd('carto'):
@@ -89,6 +85,6 @@ def create(project):
   copy_img()
   create_xml(project)
   update_renderd_conf(project)
-  update_cron(project)
+  update_tiles_list(project)
   deploy(project)
 
