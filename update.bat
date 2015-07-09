d1d6594c109c924aa01b8e7f74d6104be84b70d5
Kris Carle <kristoforcarle@moabi.org>
2015-07-09 17:59:07 -0400

    vagrant data update scripts

--- update.bat
+++ update.bat
@@ -1,1 +1,4 @@
-vagrant ssh -c vagrantupdate.sh
+vagrant ssh -c "wget https://raw.githubusercontent.com/crowdcover/carto/master/vagrantupdate.sh?token=ACUaUjt334wT9BqpPqBVOzfxcjo0BXxKks5VqCbuwA%3D%3D"
+vagrant ssh -c "mv vagrantupdate.sh?token* vagrantupdate.sh"
+vagrant ssh -c "chmod +x vagrantupdate.sh"
+vagrant ssh -c "./vagrantupdate.sh"
