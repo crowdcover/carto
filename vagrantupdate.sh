1fe9fbd5ce0d73a449e58135d2c9287a530fc23f
Kristofor Carle <kristoforcarle@moabi.org>
2015-07-20 16:52:44 -0400

    fix style path

--- vagrantupdate.sh
+++ vagrantupdate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm -f planet.latest.osm.gz
 wget http://planet.osm.moabi.org/planet.latest.osm.gz
-osm2pgsql --create --hstore-all --multi-geometry --slim --cache 64 --cache-strategy sparse --database moabi --style moabi.style planet.latest.osm.gz
+osm2pgsql --create --hstore-all --multi-geometry --slim --cache 64 --cache-strategy sparse --database moabi --style carto/moabi.style planet.latest.osm.gz
